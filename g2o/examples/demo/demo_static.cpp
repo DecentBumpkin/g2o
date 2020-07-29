f19bcdcb7298fc62aeeea5bbcc411882e6657800
Wei Jian <weiofjian@gmail.com>
2020-07-29 16:43:41 -0700

    added json I/O for g2o demo_static

--- g2o/examples/demo/demo_static.cpp
+++ g2o/examples/demo/demo_static.cpp
@@ -15,48 +15,40 @@
 
 #include "rapidjson/reader.h"
 #include "rapidjson/document.h"
+#include "rapidjson/prettywriter.h"
 #include "rapidjson/filereadstream.h"
+#include "rapidjson/filewritestream.h"
 
 using namespace Eigen;
 using namespace std;
 
-
+int readPointsJSON(const char* name, std::vector<Eigen::Vector3d>& points);
+int writePointsJSON(const char* name, const std::vector<Eigen::Vector3d>& points);
+void convertFromLidarToWorld(const std::vector<Eigen::Vector3d>& points_src,  
+                            std::vector<Eigen::Vector3d>& points_final, 
+                            const g2o::SE3Quat& T);
 /* the only edge used in this example is EdgeProjectXYZ2UV, its linearizeOplus() defined in types_six_dof_expmap.cpp */
 int main(int argc, const char* argv[]) {
 
-    FILE* fp = fopen("/home/wei/PolyExplore/LivoxStatic/build/tmptmp.json", "r"); // non-Windows use "r"
-    char readBuffer[65536];
-    rapidjson::FileReadStream is(fp, readBuffer, sizeof(readBuffer));
+    printf("%d %s\n", argc, argv[0]); /* TODO Parse Commandline*/
     
-    rapidjson::Document doc;
-    doc.ParseStream(is);
-    fclose(fp);
-
-    std::vector<Eigen::Vector3d> xyz_l;
-    if (doc.HasMember("points") && doc["points"].IsArray())
+    std::vector<Eigen::Vector3d> xyz_l, xyz_w;
+    if(readPointsJSON("/home/wei/PolyExplore/LivoxStatic/build/points_in_lidar_frame.json", xyz_l))
     {
-        printf("reading lvx %u picked points...\n", doc["points"].Size());
-        const rapidjson::Value &array = doc["points"];
-        for(auto it = array.Begin(); it != array.End(); it++){
-        
-            if(it->HasMember("x") && (*it)["x"].IsDouble() &&
-                it->HasMember("y") && (*it)["y"].IsDouble() &&
-                it->HasMember("z") && (*it)["z"].IsDouble() ){
-                printf("Read point %lf %lf %lf\n",(*it)["x"].GetDouble(),(*it)["y"].GetDouble(),(*it)["z"].GetDouble());
-                xyz_l.emplace_back((*it)["x"].GetDouble(),(*it)["y"].GetDouble(),(*it)["z"].GetDouble());
-            }
-        }
-
+        printf("Read lidar_frame failure!");
+        return 1;
+    }
+    std::vector<int> indices{1,2,3,4,5,6,7,8,10};
+    if(readPointsJSON("/home/wei/PolyExplore/LivoxStatic/scripts/total_station_xyz.json", xyz_w))
+    {
+        printf("Read world_frame failure!");
+        return 1;
     }
 
-
-    printf("%d %s\n", argc, argv[0]);
-    // int numIteration = atoi(argv[1]);
     g2o::SparseOptimizer optimizer;
     optimizer.setVerbose(false);
     std::unique_ptr<g2o::BlockSolver_6_3::LinearSolverType> linearSolver; /* 6-dof state,  3-dof meas. */
     linearSolver = g2o::make_unique<g2o::LinearSolverCholmod<g2o::BlockSolver_6_3::PoseMatrixType>>();
-
 
     g2o::OptimizationAlgorithmLevenberg* solver = new g2o::OptimizationAlgorithmLevenberg( /* use Levenberg */
         g2o::make_unique<g2o::BlockSolver_6_3>(std::move(linearSolver))
@@ -70,7 +62,7 @@ int main(int argc, const char* argv[]) {
         Eigen::AngleAxisd(0.6, Vector3d::UnitZ());
     g2o::SE3Quat T_wl_guess(q_guess, p_guess);
     // Eigen::Vector3d p_temp(-2.016, 3.546, 0.829);
-    // Eigen::Quaterniond q_temp(Eigen::AngleAxisd(-0.6, Vector3d::UnitZ()).toRotationMatrix());
+    // Eigen::Quaterniond q_temp(Eigen::AngleAxisd(-0.6, Vector3d::UnitZ()).toRotationMatrix()); /* operator= not supported */
     // g2o::SE3Quat T_wl_guess(q_temp, p_temp);
 
     std::cout << "Nominal Imu->Lidar SE3: \n" << T_wl_guess <<'\n';
@@ -80,30 +72,15 @@ int main(int argc, const char* argv[]) {
     v_se3_wl->setEstimate(T_wl_guess.inverse());
     optimizer.addVertex(v_se3_wl);
 
-/*real pain to hard code*/
-    double xyz_w_arr[9][3] = {
-        {4.8682, 3.9116,  1.9851},
-        {4.8819, 2.4407,  1.3232},
-        {4.8967, 1.0257,  1.4942},
-        {4.9001, 0.5605,  2.1421},
-        {4.9218, -1.3016,  1.8082},
-        {4.9380, -3.2454,  1.8852},
-        {4.9760,   -6.2514,   1.9442},
-        {3.7604,  -7.5817,   2.5301},
-        {-0.3135,-1.4014,  2.4282 }
-    };
-
     int vertex_id = 0; /* 0 is reserved for v_se3_il */
     for (size_t i = 0; i < 9; i++ )
     {
         g2o::VertexPointXYZ * v_xyz_w = new g2o::VertexPointXYZ();
-        Eigen::Map<const Eigen::Vector3d> xyz_w_map(xyz_w_arr[i]);
-        Eigen::Vector3d xyz_w_vec(xyz_w_map);
-        std::cout << xyz_w_vec.transpose() << std::endl;
+        std::cout << xyz_w[indices[i]].transpose() << std::endl;
         
         v_xyz_w->setId(++vertex_id);
         v_xyz_w->setFixed(true); /* fix the first camera pose */
-        v_xyz_w->setEstimate(xyz_w_vec); 
+        v_xyz_w->setEstimate(xyz_w[indices[i]]); 
         optimizer.addVertex(v_xyz_w); 
 
 
@@ -125,17 +102,86 @@ int main(int argc, const char* argv[]) {
     std::cout << " Initial Guess : \n" << T_wl_guess << '\n';
     std::cout << " Final Result  : \n" << T_final  << '\n';
     for(int i = 0;i < 9; i++){
-        Eigen::Map<const Eigen::Vector3d> xyz_w_map(xyz_w_arr[i]);
-        Eigen::Vector3d xyz_w(xyz_w_map);
-        std::cout << "total station: " << xyz_w.transpose() << std::endl;
+        std::cout << "total station: " << xyz_w[indices[i]].transpose() << std::endl;
         std::cout << "livox aligned: " << T_final.map(xyz_l[i]).transpose() << std::endl;
-        printf("error: %.5lf\n", (xyz_w - T_final.map(xyz_l[i])).norm());
-
+        printf("error: %.5lf\n", (xyz_w[indices[i]] - T_final.map(xyz_l[i])).norm());
     }
 
     std::cout << "Final Transformation:\n" << T_final << std::endl;
     Eigen::Vector3d lidar(-2.016, 3.546, 0.829);
-    std::cout << "livox fixture: " << T_final.inverse().map(lidar).transpose() << std::endl;
-    std::cout << "livox fixture: " << T_final.inverse().map(lidar).norm() << std::endl;
+    std::cout << "Meausred lidar origin in lidar_frame: \n" << T_final.inverse().map(lidar).transpose() << std::endl;
+    std::cout << "Meausred lidar origin in lidar_frame: " << T_final.inverse().map(lidar).norm() << std::endl;
+    
+    std::vector<Eigen::Vector3d> xyz_final;
+    convertFromLidarToWorld(xyz_l, xyz_final, T_final);
+    writePointsJSON("copy_of_world.json", xyz_final);
+    
+}
 
+void convertFromLidarToWorld(const std::vector<Eigen::Vector3d>& points_src,  
+                            std::vector<Eigen::Vector3d>& points_final, 
+                            const g2o::SE3Quat& T){
+    points_final.clear();
+    for(auto& point_src : points_src){
+        points_final.push_back(T.map(point_src));
+    }
+    assert(point_src.size() == points_final.size());
+}
+
+int writePointsJSON(const char* name, const std::vector<Eigen::Vector3d>& points){
+
+    rapidjson::Document doc;
+    doc.SetObject();
+    rapidjson::Value points_js = rapidjson::Value(rapidjson::kArrayType);
+    for(auto& point : points)
+    {
+        rapidjson::Value item(rapidjson::kObjectType);
+        rapidjson::Value point_js(rapidjson::kObjectType);
+        item.SetDouble(point[0]);
+        point_js.AddMember("x", item, doc.GetAllocator());
+        item.SetDouble(point[1]);
+        point_js.AddMember("y", item, doc.GetAllocator());
+        item.SetDouble(point[2]);
+        point_js.AddMember("z", item, doc.GetAllocator()); /* item is moved */
+        points_js.PushBack(point_js, doc.GetAllocator()); /* point is moved */
+    }
+    doc.AddMember("points", points_js, doc.GetAllocator() ); /* points is "moved" */
+    // save to json format
+    FILE* fp = fopen(name, "w"); // non-Windows use "w"
+    if(!fp) return 1;
+    char writeBuffer[65536];
+    rapidjson::FileWriteStream os(fp, writeBuffer, sizeof(writeBuffer));
+    rapidjson::PrettyWriter<rapidjson::FileWriteStream> fwriter(os);
+    fwriter.SetMaxDecimalPlaces(6);
+    doc.Accept(fwriter);
+    fclose(fp);
+
+    return 0;
+}
+
+int readPointsJSON(const char* name, std::vector<Eigen::Vector3d>& points){
+    FILE* fp = fopen(name, "r"); // non-Windows use "r"
+    if(!fp) return 1;
+    char readBuffer[65536];
+    rapidjson::FileReadStream is(fp, readBuffer, sizeof(readBuffer));
+    rapidjson::Document doc;
+    doc.ParseStream(is);
+
+    points.clear();
+    if (doc.HasMember("points") && doc["points"].IsArray())
+    {
+        printf("reading lvx %u picked points...\n", doc["points"].Size());
+        const rapidjson::Value &array = doc["points"];
+        for(auto it = array.Begin(); it != array.End(); it++){
+        
+            if(it->HasMember("x") && (*it)["x"].IsDouble() &&
+                it->HasMember("y") && (*it)["y"].IsDouble() &&
+                it->HasMember("z") && (*it)["z"].IsDouble() ){
+                printf("Read point %lf %lf %lf\n",(*it)["x"].GetDouble(),(*it)["y"].GetDouble(),(*it)["z"].GetDouble());
+                points.emplace_back((*it)["x"].GetDouble(),(*it)["y"].GetDouble(),(*it)["z"].GetDouble());
+            }
+        }
+    }
+    fclose(fp);
+    return 0;
 }
